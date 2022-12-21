74046625089670ace0e30fe95867f6a17618c8fc
Albin Varillas <81118732+Albin-Varillas@users.noreply.github.com>
2022-12-21 05:11:56 +0100

    completed

---
+++ uppgifter/mongodb-uppgifter/index.js
@@ -0,0 +1,53 @@
+const { MongoClient } = require("mongodb");
+
+const url =
+  "mongodb+srv://albin:albin@cluster0.sh8d5o1.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
+async function main() {
+  await client.connect();
+  const db = client.db("test");
+  const users =
+    //-------hitta alla som bor i stockholm----------
+    //   const users = await db
+    //     .collection("users")
+    //     .find({
+    //       city: "Stockholm",
+    //     })
+    //     .toArray();
+    //   console.log(users);
+
+    // --------lägga till 5 users----------------
+    //   await db.collection("users").insertMany([
+    //     { name: "Gilbert", age: 5, city: "Stockholm" },
+    //     { name: "Adam", age: 19, city: "Stockholm" },
+    //     { name: "Emma", age: 12, city: "Kalmar" },
+    //     { name: "Fredrik", age: 45, city: "Malmö" },
+    //     { name: "Mia", age: 29, city: "Göteborg" },
+    //   ]);
+
+    //   --------updatera Gilberts city-----------
+    //     await db.collection("users").updateOne(
+    //       {
+    //         name: "Gilbert",
+    //         city: "Stockholm",
+    //       },
+    //       {
+    //         $set: { city: "Malmö" },
+    //       }
+    //     );
+
+    // --------hitta andvändare över 18 år--------
+    //     await db
+    //       .collection("users")
+    //       .find({ age: { $gte: 18 } })
+    //       .toArray();
+    //   console.log(users);
+
+    // --------Hämta alla användare och sortera  namn i alfabetiskt ordning
+
+    await db.collection("users").find().sort({ name: 1 }).toArray();
+  console.log(users);
+}
+
+main();
